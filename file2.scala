08346c13058ec5330709d15d3e410a49a3732140
Hyojoo <kangh83@zedat.fu-berlin.de>
2024-08-19 17:00:55 +0200

    add tail_recursion

--- file2.scala
+++ file2.scala
@@ -2,6 +2,14 @@
 
 def fac (n:Int):Int =
   if n == 0 then 1 else n*fac(n-1)
+
 def gcd(a:Int, b:Int):Int =
     if b == 0 then a else gcd(b, a%b)
+
+
+/*Tail Recursion*/
+@tailrec
+def tail_fac(n:Int, acc:Int):Int =
+  if (n <=1) acc
+  else fac(n-1, n*acc)
 
